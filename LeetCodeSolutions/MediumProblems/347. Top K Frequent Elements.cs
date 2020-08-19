e4456d1600a2061837846e83a63f07da3bf89168
Khaled Saleh <dev.KhaledSaleh@gmail.com>
2020-08-19 05:26:09 -0500

    Update 347. Top K Frequent Elements.cs

--- LeetCodeSolutions/MediumProblems/347. Top K Frequent Elements.cs
+++ LeetCodeSolutions/MediumProblems/347. Top K Frequent Elements.cs
@@ -5,20 +5,21 @@ namespace LeetCodeSolutions.MediumProblems
 {
 	public class TopKFrequentElements
 	{
+		
 		public int[] Solve(int [] nums, int k)
-        {
-            var map = new Dictionary<int, int>();
+		{
+		    var map = new Dictionary<int, int>();
 
-            foreach (int n in nums)
-            {
-                if (!map.ContainsKey(n))
-                    map.Add(n, 1);
-                else
-                    map[n]++;
-            }
+		    foreach (int n in nums)
+		    {
+			if (!map.ContainsKey(n))
+			    map.Add(n, 1);
+			else
+			    map[n]++;
+		    }
 
-            return map.OrderByDescending(c => c.Value)
-                .Take(k).Select(c => c.Key).ToArray();
-        }
+		    return map.OrderByDescending(c => c.Value)
+			.Take(k).Select(c => c.Key).ToArray();
+		}
     }
 }
