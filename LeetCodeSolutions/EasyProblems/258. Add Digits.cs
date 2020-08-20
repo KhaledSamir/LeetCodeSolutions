863c60d23b4c7b119f6d01b50ab59e81427485e4
Khaled Saleh <dev.khaledsaleh@gamil.com>
2020-08-20 08:56:03 -0500

    Added AddDigits problem solution.

---
+++ LeetCodeSolutions/EasyProblems/258. Add Digits.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace LeetCodeSolutions.EasyProblems
+{
+    public class AddDigits58
+    {
+        // Recursion solution
+
+        public int Solve(int num)
+        {
+            int sum = 0;
+
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+
+            return sum >= 10 ? Solve(num) : sum;
+        }
+
+        // Iterative solution
+
+        //public int Solve(int num)
+        //{
+
+        //    while (num >= 10)
+        //    {
+        //        int sum = 0;
+        //        while (num > 0)
+        //        {
+        //            sum += num % 10;
+        //            num /= 10;
+        //        }
+        //        num = sum;
+        //    }
+
+
+        //    return num;
+        //}
+    }
+}
